70f450f2f43242904cb0de38af0a6aed67f40e69
Ashish Rathod <ashish.rathod@auxanoglobalservices.com>
2023-01-17 16:05:59 +0530

    170120231605

--- APlus/Controller/FirstVC/FirstVC.swift
+++ APlus/Controller/FirstVC/FirstVC.swift
@@ -40,7 +40,7 @@ public class FirstVC: UIViewController {
     var arrRecentChatUserList : [GetUserList]? = []
     private var imageRequest: Cancellable?
     var profileDetail : ProfileDetail?
-    public var hideTopView : Bool = false
+    public var hideTopView : Bool = false   //  for hide top bar from chat.
     
     let activityIndicator = UIActivityIndicatorView()
     
